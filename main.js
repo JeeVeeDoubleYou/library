d7e424fd259a60eea8411b06c0093d2bb081e085
Gaëlle Viso <gaelleviso@gmail.com>
2024-07-03 12:44:39 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${read ? 'already read' : 'not read yet'}`;
+    }
+}
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+console.log(book1.info());
+
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function() {
+      console.log(this.name)
+    };
+  }
+  
+  const player1 = new Player('steve', 'X');
+  const player2 = new Player('also steve', 'O');
