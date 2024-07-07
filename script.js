d5afa61cb46242d5a03f94152d5ffd1e6683e40b
Gaëlle Viso <gaelleviso@gmail.com>
2024-07-07 16:10:05 +0200

    Consolidated all html into index and all js into script. Form and library work.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 const myLibrary = [
-    new Book('The Hobbit', 'J.R.R. Tolkien', 295, true),
-    new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 398, true),
-    new Book('The Two Towers', 'J.R.R. Tolkien', 327, false),
-    
 ];
 
 function Book(title, author, pages, read){
@@ -18,6 +14,40 @@ function Book(title, author, pages, read){
 function displayBooks(){
     const bookList = document.getElementById('bookList');
     myLibrary.forEach(book => {bookList.innerHTML += `<div class="book">${book.info()}</div>`});
+}
+
+function showForm(){
+    const form = document.getElementById('newBookForm');
+    form.style.display = "";
+    const library = document.getElementById('libraryPage');
+    library.style.display = "none";
+}
+
+function hideForm(){
+    const form = document.getElementById('newBookForm');
+    form.style.display = "none";
+    const library = document.getElementById('libraryPage');
+    library.style.display = "";
+}
+
+document.getElementById('newBookForm').addEventListener('submit', function(e) {
+    const form = document.getElementById('newBookForm');
+    e.preventDefault();
+    addBookToLibrary(); 
+    hideForm();
+    const bookList = document.getElementById('bookList');
+    bookList.innerHTML = '';
+    displayBooks();
+    form.reset();
+});
+
+function addBookToLibrary(){
+    const form = document.getElementById('newBookForm');
+    const title = form.elements['title'].value;
+    const author = form.elements['author'].value;
+    const pages = form.elements['pages'].value;
+    const read = form.elements['read'].checked;
+    myLibrary.push(new Book(title, author, pages, read));
 }
 
 displayBooks();
