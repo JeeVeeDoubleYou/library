eecf349f7c13f4586ed6d5c852efe0e9e7d46df0
Gaëlle Viso <gaelleviso@gmail.com>
2024-07-11 16:04:44 +0200

    Added toggle switch between read and not read

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        return `${title} by ${author}, ${pages} pages, ${read ? 'already read' : 'not read yet'}`;
+    switchReadStatus = function(){
+        this.read = !this.read;
     }
+
+    getBookInfo = function(){
+        return `${title} by ${author}, ${pages} pages`;
+    }
+
+    return {switchReadStatus, getBookInfo, read};
 }
 
 function displayBooks(){
     const bookList = document.getElementById('bookList');
     bookList.innerHTML = '';
     bookNumber = 0;
-    myLibrary.forEach(book => {bookList.innerHTML += `<div class="book"><div class="bookInfo">${book.info()}</div><button class="deleteBookBtn" onclick="deleteBook(this.id)" id="${bookNumber}">Delete Book</button></div>`; bookNumber++});
+    const readBtns = document.getElementsByClassName('readBtn');
+    myLibrary.forEach(book => {bookList.innerHTML += `<div class="book"><div class="bookInfo">${book.getBookInfo()}</div><button class="readBtn" onclick="changeReadStatus(this.id)" id="${bookNumber}"></button><button class="deleteBookBtn" onclick="deleteBook(this.id)" id="${bookNumber}">Delete Book</button></div>`; if(book.read){readBtns[bookNumber].innerHTML=`Read`}else{readBtns[bookNumber].innerHTML=`Not Read`};bookNumber++});
 }
 
 function showForm(){
@@ -46,12 +53,17 @@ function addBookToLibrary(){
     const title = form.elements['title'].value;
     const author = form.elements['author'].value;
     const pages = form.elements['pages'].value;
-    const read = form.elements['read'].checked;
+    let read = form.elements['read'].checked;
     myLibrary.push(new Book(title, author, pages, read));
 }
 
 function deleteBook(clicked_id){
     myLibrary.splice(clicked_id, 1);
+    displayBooks();
+}
+
+function changeReadStatus(clicked_id){
+    myLibrary[clicked_id].switchReadStatus();
     displayBooks();
 }
 
